8d47162c27d78b6077b13b83422d9440ae5d8004
winstonbarbe <winston.barbe@gmail.com>
2024-12-29 12:07:17 -0500

    adds instance variable to hello action and hello view

--- app/controllers/api/examples_pages_controller.rb
+++ app/controllers/api/examples_pages_controller.rb
@@ -1,5 +1,6 @@
 class Api::ExamplesPagesController < ApplicationController
   def hello_action
+    @message = "Hello from instance variable"
     render template: "api/examples_pages/hello"
   end
 end
